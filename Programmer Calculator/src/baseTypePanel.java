27908d000d89b9a773fc3cf88492ef745b93ce8c
Kion Smith <kion_smith@hotmail.com>
2017-11-15 13:40:35 -0600

    added basic things for base panel

--- Programmer Calculator/src/baseTypePanel.java
+++ Programmer Calculator/src/baseTypePanel.java
@@ -1,9 +1,37 @@
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+
+import javax.swing.ButtonGroup;
 import javax.swing.JPanel;
 import javax.swing.JRadioButton;
 
 public class baseTypePanel extends JPanel
 {
 	JRadioButton hexButton,decButton,octButton,binButton;
+	ButtonGroup baseType;
+	
+	public baseTypePanel()
+	{
+		hexButton = new JRadioButton("Hex");
+		decButton = new JRadioButton("Dec");
+		octButton = new JRadioButton("Oct");
+		binButton = new JRadioButton("Bin");
+		
+		baseType.add(hexButton);
+		baseType.add(decButton);
+		baseType.add(octButton);
+		baseType.add(binButton);
+		
+		setLayout(new GridBagLayout());
+		GridBagConstraints gc = new GridBagConstraints();
+		
+
+		add(hexButton,gc);
+		add(decButton,gc);
+		add(octButton,gc);
+		add(binButton,gc);
+	}
+	
 	//getters
 	public JRadioButton getHexButton() 
 	{
