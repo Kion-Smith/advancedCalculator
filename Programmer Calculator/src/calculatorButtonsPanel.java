2f6959756d275f8aee83335962ffbd0608fd5690
Kion-Smith <kion_smith@hotmail.com>
2017-11-16 21:54:44 -0600

    adding setters and getters

--- Programmer Calculator/src/calculatorButtonsPanel.java
+++ Programmer Calculator/src/calculatorButtonsPanel.java
@@ -7,4 +7,295 @@ public class calculatorButtonsPanel extends JPanel
 			a_Button,b_Button,c_Button,d_Button,e_Button,f_Button,//Hex buttons
 			addButton,subButton,multButton,divButton,signButton,equalsButton,modButton,
 			decimalButton,sqrtButton,percentButton,recipButton; //all disable buttons
+	
+	
+
+	public calculatorButtonsPanel()
+	{
+		
+	}
+	//Getters
+	public JButton getZeroButton() 
+	{
+		return zeroButton;
+	}
+
+
+	public JButton getOneButton() 
+	{
+		return oneButton;
+	}
+
+
+	public JButton getTwoButton() 
+	{
+		return twoButton;
+	}
+
+
+	public JButton getThreeButton() 
+	{
+		return threeButton;
+	}
+
+
+	public JButton getFourButton() 
+	{
+		return fourButton;
+	}
+
+
+	public JButton getFiveButton() 
+	{
+		return fiveButton;
+	}
+
+	public JButton getSixButton() 
+	{
+		return sixButton;
+	}
+
+
+	public JButton getSevenButton() 
+	{
+		return sevenButton;
+	}
+
+
+	public JButton getEightButton() 
+	{
+		return eightButton;
+	}
+
+
+	public JButton getNineButton() 
+	{
+		return nineButton;
+	}
+
+
+	public JButton getA_Button() 
+	{
+		return a_Button;
+	}
+
+
+	public JButton getB_Button() 
+	{
+		return b_Button;
+	}
+
+
+	public JButton getC_Button() 
+	{
+		return c_Button;
+	}
+
+
+	public JButton getD_Button() {
+		return d_Button;
+	}
+
+
+	public JButton getE_Button() {
+		return e_Button;
+	}
+
+
+	public JButton getF_Button() {
+		return f_Button;
+	}
+
+
+	public JButton getAddButton() {
+		return addButton;
+	}
+
+
+	public JButton getSubButton() {
+		return subButton;
+	}
+
+
+	public JButton getMultButton() {
+		return multButton;
+	}
+
+
+	public JButton getDivButton() {
+		return divButton;
+	}
+
+
+	public JButton getSignButton() {
+		return signButton;
+	}
+
+
+	public JButton getEqualsButton() {
+		return equalsButton;
+	}
+
+
+	public JButton getModButton() {
+		return modButton;
+	}
+
+
+	public JButton getDecimalButton() {
+		return decimalButton;
+	}
+
+
+	public JButton getSqrtButton() {
+		return sqrtButton;
+	}
+
+
+	public JButton getPercentButton() {
+		return percentButton;
+	}
+
+
+	public JButton getRecipButton() {
+		return recipButton;
+	}
+
+	//setters
+	public void setZeroButton(JButton j) 
+	{
+		zeroButton = j;
+	}
+
+	public void setOneButton(JButton j) 
+	{
+		oneButton = j;
+	}
+
+	public void setTwoButton(JButton j) 
+	{
+		twoButton = j;
+	}
+
+	public void setThreeButton(JButton j) 
+	{
+		threeButton = j;
+	}
+
+	public void setFourButton(JButton j)
+	{
+		fourButton = j;
+	}
+
+	public void setFiveButton(JButton j) 
+	{
+		fiveButton = j;
+	}
+
+	public void setSixButton(JButton j) 
+	{
+		sixButton = j;
+	}
+
+	public void setSevenButton(JButton j) 
+	{
+		sevenButton = j;
+	}
+
+	public void setEightButton(JButton j) 
+	{
+		eightButton = j;
+	}
+
+	public void setNineButton(JButton j) 
+	{
+		nineButton = j;
+	}
+
+	public void setA_Button(JButton j) 
+	{
+		a_Button = j;
+	}
+
+	public void setB_Button(JButton j) 
+	{
+		b_Button = j;
+	}
+
+	public void setC_Button(JButton j) 
+	{
+		c_Button = j;
+	}
+
+	public void setD_Button(JButton j) 
+	{
+		d_Button = j;
+	}
+
+	public void setE_Button(JButton j) 
+	{
+		e_Button = j;
+	}
+
+	public void setF_Button(JButton j) 
+	{
+		f_Button = j;
+	}
+
+	public void setAddButton(JButton j) 
+	{
+		addButton = j;
+	}
+
+	public void setSubButton(JButton j) 
+	{
+		subButton = j;
+	}
+
+	public void setMultButton(JButton j) 
+	{
+		multButton = j;
+	}
+
+	public void setDivButton(JButton j) 
+	{
+		divButton = j;
+	}
+
+	public void setSignButton(JButton j) 
+	{
+		signButton = j;
+	}
+
+	public void setEqualsButton(JButton j) 
+	{
+		equalsButton = j;
+	}
+
+	public void setModButton(JButton j) 
+	{
+		modButton = j;
+	}
+
+	public void setDecimalButton(JButton j) 
+	{
+		decimalButton = j;
+	}
+
+	public void setSqrtButton(JButton j) 
+	{
+		sqrtButton = j;
+	}
+
+	public void setPercentButton(JButton j) 
+	{
+		percentButton = j;
+	}
+
+	public void setRecipButton(JButton j) 
+	{
+		recipButton = j;
+	}
+
+	
 }
